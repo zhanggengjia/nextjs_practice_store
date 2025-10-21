8f80f21a488ba7638523abc7233442a13507f3d1
zhanggengjia <73398123+zhanggengjia@users.noreply.github.com>
2025-10-21 19:30:36 +0800

    update

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -4,12 +4,12 @@ import HeroCarousel from './HeroCarousel';
 
 const Hero = () => {
   return (
-    <section className="grid grid-cols-1 lg:grid-cols-2 gap-24 items-center">
+    <section className="grid grid-cols-1 lg:grid-cols-2 gap-24 items-center ">
       <div>
-        <h1 className="max-w-2xl font-bold text-4xl tracking-tight sm:text-6xl">
+        <h1 className="max-w-2xl font-bold text-4xl tracking-tight sm:text-6xl ">
           Design sustainably. Build intelligently.
         </h1>
-        <p className="mt-8 max-w-xl text-lg leading-8">
+        <p className="mt-8 max-w-xl text-lg leading-8 ">
           A web platform connecting designers with reusable building materials
           from soon-to-be-demolished structures — powered by Next.js,
           TypeScript, and Supabase/PostgreSQL to make material reuse searchable,
